0cdc4202ce67fac46361f8b06e18e2342f05e273
User <User@C-A01>
2022-08-19 16:37:37 +0900

    220819(금) 자바 수업 - 예제 코드(5) static method
    1. static 메소드는 클래스명.메소드명으로 호출할 수 있다.
    2. 즉, 객체를 생성하지 않고도 getData(); 호출이 가능하다는 의미

---
+++ src/chap09/Code09.java
@@ -0,0 +1,12 @@
+package chap09;
+
+public class Code09 {
+
+	public static void main(String[] args) {
+		
+		/* static 메소드는 클래스명.메소드명으로 호출할 수 있다. */
+		System.out.println("WithStatic.getData() : " + WithStatic.getData());
+		
+	}
+
+}
---
+++ src/chap09/WithStatic.java
@@ -0,0 +1,18 @@
+package chap09;
+
+public interface WithStatic {
+
+	/* fields */
+	
+
+	/* constructors */
+
+
+	/* methods */
+	int get();
+	
+	static int getData() {
+		return 100;
+	}
+	
+}
