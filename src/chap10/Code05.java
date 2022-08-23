e4d2bf8082d70ec36fb9d60e0746b32aa5d84a58
50hwang <fiftyhwang50@knou.ac.kr>
2022-08-23 15:07:26 +0900

    220823 객체.equals(객체) 복습

---
+++ src/chap10/Code05.java
@@ -0,0 +1,24 @@
+package chap10;
+
+public class Code05 {
+
+	public static void main(String[] args) {
+		
+		String x = new String("Hello world");
+		String y = new String("Hello world");
+		
+		if ( x == y ) {
+			System.out.println("x == y");
+		} else {
+			System.out.println("x != y");
+		}
+		
+		if (x.equals(y)) {
+			System.out.println("x equals y");
+		} else {
+			System.out.println("x not equals y");
+		}
+		
+	}
+
+}
